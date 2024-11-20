98fc9df62e1a7bc40f3ac9066447ad6b136896b6
FGVargas <vargasmarquezjosemanuel@gmail.com>
2024-11-19 22:05:18 -0600

    restruct carpets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./dashboard/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'login',
-    loadComponent: () => import('./componentes/login/login.page').then((m) => m.LoginPage)
+    loadComponent: () => import('./componentes/auth/login/login.page').then((m) => m.LoginPage)
   },
   {
-    path: '',
+    path: '**',
     redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'login',
-    loadComponent: () => import('./componentes/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./componentes/auth/login/login.page').then( m => m.LoginPage)
   },
 ];
