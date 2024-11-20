cd950899806c5ce413915d0fc6861016083e7eee
sergioeling <20207487@uatx.mx>
2024-11-19 23:04:46 -0600

    UI

--- src/app/componentes/auth/login/login.page.ts
+++ src/app/componentes/auth/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonSkeletonText, IonText } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common'; 
+import { IonicModule } from '@ionic/angular'; // Asegúrate de importar IonicModule
 
 
 @Component({
@@ -9,13 +11,16 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonSkeletonText, 
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonText, IonSkeletonText, IonItem, IonHeader, IonToolbar, IonTitle, IonContent,
-    ReactiveFormsModule
+  imports: [
+    IonicModule,  // Esto ya incluye todos los componentes necesarios de Ionic
+    ReactiveFormsModule, 
+    CommonModule
 ],
 })
 export class LoginPage implements OnInit {
 
   protected fgLogin: FormGroup;
+  public loading: boolean = false;
 
   constructor(private fBuilder: FormBuilder, private router: Router) {
     this.fgLogin = fBuilder.group({
@@ -25,10 +30,18 @@ export class LoginPage implements OnInit {
     })
   }
 
-  login(){
-    console.log("login!")
-    this.router.navigate(["/home"]);
+  login() {
+    this.loading = true;
+    // Simular tiempo de carga (reemplazar con tu lógica real de login)
+    setTimeout(() => {
+      this.loading = false;
+      // Tu lógica de login aquí
+      this.router.navigate(["/home"]);
+    }, 2000);
+   
   }
+
+ 
 
   ngOnInit() {
   }
