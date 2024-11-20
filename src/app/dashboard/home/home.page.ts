95621f1b45bedc47f4f0b1761dad51426e461004
aanavrin268 <aanavrin268@gmail.com>
2024-11-19 22:52:25 -0600

    ca

--- src/app/dashboard/home/home.page.ts
+++ src/app/dashboard/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonSkeletonText, IonText } from '@ionic/angular/standalone';
-import { HeadersComponent } from '../componentes/shared/headers/headers.component';
 import { CommonModule } from '@angular/common';
+import { HeadersComponent } from '../shared/headers/headers.component';
 
 @Component({
   selector: 'app-home',
@@ -22,6 +22,11 @@ export class HomePage {
     this.list_paquetes = [
       {title: "6 Rebanadas", text: "2x1 Chica", price: "215"},
       {title: "8 Rebanadas", text: "2x1 Mediana", price: "245"},
+      {title: "12 Rebanadas", text: "2x1 Grande", price: "285"},
+      {title: "16 Rebanadas", text: "2x1 Familiar", price: "330"},
+      {title: "22 Cuadros", text: "2x1 Mega", price: "399"},
+
+
 
   ]
   }
