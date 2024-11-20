d1fdc95c9814413e72e45744b7cc09fce6679580
FGVargas <vargasmarquezjosemanuel@gmail.com>
2024-11-19 22:14:54 -0600

    restruct carpets

--- src/app/dashboard/shopping-cart/shopping-cart.component.ts
+++ src/app/dashboard/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss'],
   standalone: true
 })
-export class ShoppingCartComponent   {
+export class ShoppingCartComponent {
 
-  constructor() { }
-
-
+  constructor() {
+  }
 
 }
